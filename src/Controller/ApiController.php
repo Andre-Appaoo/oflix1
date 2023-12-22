94faf72386b1af9286c4aa366185417095485e18
andre-1985 <andre.appaoo@gmail.com>
2023-12-22 12:52:42 +0100

    Modifications dans ApiController

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -6,8 +6,9 @@ use App\Repository\ArtworkRepository;
 use App\Repository\GenreRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/api', name: 'app_api-')]
 class ApiController extends AbstractController
 {
     private GenreRepository $genreRepository;
@@ -19,27 +20,31 @@ class ApiController extends AbstractController
         $this->artworkRepository = $artworkRepository;
     }
 
-    #[Route('/api/artworks', name: 'app_api-artworks')]
+    #[Route('/api/artworks', name: 'artworks')]
     public function getAllArtworks(): JsonResponse
     {
         return $this->json($this->artworkRepository->findAll());
     }
 
-    #[Route('/api/artworks/{id}', name: 'app_api-artwork')]
+    #[Route('/api/artworks/{id}', name: 'artwork')]
     public function getArtwork(int $id): JsonResponse
     {
-        return $this->json($this->artworkRepository->find($id));
+        return $this->json([
+            'data' => $this->artworkRepository->find($id)
+        ]);
     }
 
-    #[Route('/api/genres', name: 'app_api-genres')]
+    #[Route('/api/genres', name: 'genres')]
     public function getAllGenres(): JsonResponse
     {
         return $this->json($this->genreRepository->findAll());
     }
 
-    #[Route('/api/genres/{id}', name: 'app_api-genre')]
+    #[Route('/api/genres/{id}', name: 'genre')]
     public function getGenre(int $id): JsonResponse
     {
-        return $this->json($this->genreRepository->find($id));
+        return $this->json([
+            'data' => $this->genreRepository->find($id)
+        ]);
     }
 }
